cdf483b567f1d6f2acb351b03bf3bc8c5f9c333f
notha99y <renjie.tan91@gmail.com>
2018-08-12 13:45:33 +0800

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# WebScraping Repo
-This repo contains my learning of the basics of webscraping and webcrawling (future)
+# Grocery Repo
+This repo contains scripts to do web scraping of e-commerce grocery web pages. <br>
+This is done merely as a school project and we adhere to the robots.txt and ethics of being a [good webscrapper](https://towardsdatascience.com/ethics-in-web-scraping-b96b18136f01). <br>
+
+# Requirements
+In order to run the code, you would need to have [Anaconda3](https://www.anaconda.com/download/) installed. 
+
+# Setup
+1. Clone the repo
+```
+git clone https://github.com/notha99y/Grocery.git
+```
+2. Set up conda environment
+```
+conda env create -f=environment.yml
+```
+
+With this, you are set to do webscraping and some simple data analysis
+
+# RedMart
+Somethings you do can with the Redmart Scripts
+
+- Scrap Redmart 
+    - This would create a project directory called data/raw, scraps the redmart webpage and saves it into a .json file
+    - This would take roughly 10 mins (depending on your internet speed and redmart servers) The .json file would be rougly 200 MB in size
+```
+python src/redmart.py
+```
+- Extract data into MongoDB
+    - This would extract the relevant information from the raw json data and add it into MongoDB. 
+    - The extracted data will be saved into a db called Grocery and in a collection called redmart
+```
+python src/make_mongodb_redmart.py
+```
+
+# FairPrice
+TODO
--- src/make_mongodb_redmart.py
+++ src/make_mongodb_redmart.py
@@ -1,8 +1,44 @@
 import os
 import glob
-from pymongo import MongoClient
+import time
 import json
-TODO
+
+from pymongo import MongoClient
 
 if __name__ == '__main__':
-    pass
+    data_path = os.path.join(os.getcwd(), 'data', 'raw')
+    raw_jsons = glob.glob(os.path.join(data_path, '*.json'))
+
+    print("Please select a raw data.json to put into mongodb")
+    for i, file in enumerate(raw_jsons):
+        print('[{}]: {}'.format(i, file))
+    selected = int(input("Input number from 0 - {}: ".format(len(raw_jsons))))
+    print("selected [{}]: {}".format(selected, raw_jsons[selected]))
+    # time.sleep(5)
+    with open(raw_jsons[selected]) as fp:
+        selected_json = json.load(fp)
+
+    client = MongoClient()
+    db = client.Grocery
+    redmart = db.redmart
+    for pdtDetails in selected_json.values():
+        IMAGE_URL = 'https://s3-ap-southeast-1.amazonaws.com/media.redmart.com/newmedia/150x'
+        try:
+            pdtOrganic = pdtDetails['filters']['is_organic']
+        except Exception as e:
+            pdtOrganic = '0'
+        row = {
+            'pdtID': pdtDetails['id'],
+            'pdtName': pdtDetails['title'],
+            'pdtDesc': pdtDetails['desc'],
+            'pdtImageURL': IMAGE_URL + str(pdtDetails['img']['name']),
+            'pdtPrice': pdtDetails['pricing']['price'],
+            'pdtCountryOfOrigin': pdtDetails['details']['country_of_origin'],
+            'pdtOrganic': pdtOrganic,
+            'pdtMfgName': pdtDetails['filters']['mfr_name'],
+            'pdtBrandName': pdtDetails['filters']['brand_name'],
+            'pdtStockStatus': pdtDetails['inventory']['stock_status'],
+            'pdtURI': pdtDetails['details']['uri'],
+            'pdtCategoryTags': pdtDetails['category_tags']
+        }
+        redmart.insert(row)
